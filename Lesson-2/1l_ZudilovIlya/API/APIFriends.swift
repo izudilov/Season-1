ed01d51e2766a3ec2c6b1591b4e6dca96fcd0673
izudilov <mrzudilov@mail.ru>
2020-12-20 17:43:37 +0100

    Домашка ко 2 уроку

---
+++ Lesson-2/1l_ZudilovIlya/API/APIFriends.swift
@@ -0,0 +1,32 @@
+//
+//  APIFriends.swift
+//  1l_ZudilovIlya
+//
+//  Created by user179996 on 20.12.2020.
+//  Copyright © 2020 izudilov. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class APIFriends {
+    
+    let baseUrl = "https://api.vk.com/method/"
+    let version = "5.126"
+    func getData(fields: String?){
+        let request = "friends.get"
+        guard let user = Session.shared.userID else {return}
+        guard let apiKey = Session.shared.acess_token else {return}
+        let parameters: Parameters = [
+            "user_ids": user,
+            "fields": fields as Any,
+            "access_token": apiKey,
+            "v": version
+        ]
+        let url = baseUrl+request
+        AF.request(url, method: .get, parameters:
+                    parameters).responseJSON { repsonse in
+                        print(repsonse.value!)
+                    }
+    }
+}
