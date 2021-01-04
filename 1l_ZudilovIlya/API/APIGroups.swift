7a524c1d675c4fe0b9e99cf0aa5e65f62bf4ff2c
izudilov <mrzudilov@mail.ru>
2021-01-04 23:23:21 +0100

    Домашка к 4 уроку

---
+++ 1l_ZudilovIlya/API/APIGroups.swift
@@ -0,0 +1,87 @@
+//
+//  APIGroups.swift
+//  1l_ZudilovIlya
+//
+//  Created by user179996 on 20.12.2020.
+//  Copyright © 2020 izudilov. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import RealmSwift
+
+class APIGroups {
+   
+    
+    let baseUrl = "https://api.vk.com/method/"
+    let version = "5.126"
+
+    
+    func loadVKGroups(completion: @escaping ([GroupVK]) -> Void ){
+        
+        let request = "groups.get"
+        
+        guard let user = Session.shared.userID else {return}
+        guard let apiKey = Session.shared.acess_token else {return}
+        
+        let parameters: Parameters = [
+            "user_ids": user,
+            "fields": "name,photo_100",
+            "access_token": apiKey,
+            "v": version,
+            "extended": "1"
+        ]
+        
+        let url = baseUrl+request
+        
+        AF.request(url, method: .get, parameters: parameters).responseJSON { response in
+                       
+            guard let data = response.data else { return }
+            
+            do {
+                let parsedResult: StructAPIGruop = try JSONDecoder().decode(StructAPIGruop.self, from: data)
+                let array = parsedResult.response.items
+                MyGroupVK.groupsFromVK = array
+               //print(array[0].name)
+                
+                do {
+                    let realm =  try Realm()
+                    realm.beginWrite()
+                    realm.add(parsedResult.response.items)
+                    try realm.commitWrite()
+                    
+                } catch {
+                    print(error)
+                }
+                completion (array)
+                
+            }
+            catch {
+                print(error)
+            }
+           
+            
+           /* switch response.result {
+               
+                        case .success:
+                                    do {
+                                        let parsedResult: StructAPI = try JSONDecoder().decode(StructAPI.self, from: response.data!)
+                                        let array = parsedResult.response.items
+                                        FriendsVK.friends = array
+                                        
+                                        return
+                                        }
+                                    catch {
+                                        print(error)
+                                    }
+                            
+                        case .failure(let error):
+                                print(error)
+            
+           
+                    }*/
+        
+        }
+        
+    }
+}
---
+++ 1l_ZudilovIlya/ViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  1l_ZudilovIlya
+//
+//  Created by user179996 on 22.10.2020.
+//  Copyright © 2020 izudilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Жест нажатия
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        // Присваиваем его UIScrollVIew
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+        
+       
+
+    }
+    
+    override func viewDidDisappear (_ animated: Bool) {
+        UserData.shared.userId = 000001
+        UserData.shared.token = "444KnvOewf312Poefff23"
+        UserData.shared.userName = loginInput.text
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    // Когда клавиатура появляется
+        @objc func keyboardWasShown(notification: Notification) {
+            
+            // Получаем размер клавиатуры
+            let info = notification.userInfo! as NSDictionary
+            let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+            let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+            
+            // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+            self.scrollView?.contentInset = contentInsets
+            scrollView?.scrollIndicatorInsets = contentInsets
+        }
+        
+        //Когда клавиатура исчезает
+        @objc func keyboardWillBeHidden(notification: Notification) {
+            // Устанавливаем отступ внизу UIScrollView, равный 0
+            let contentInsets = UIEdgeInsets.zero
+            scrollView?.contentInset = contentInsets
+        }
+
+    override func viewWillAppear(_ animated: Bool) {
+          super.viewWillAppear(animated)
+          
+          // Подписываемся на два уведомления: одно приходит при появлении клавиатуры
+          NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+          // Второе — когда она пропадает
+          NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+      }
+
+    override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
+    
+    @objc func hideKeyboard() {
+            self.scrollView?.endEditing(true)
+        }
+
+    @IBOutlet weak var loginInput: UITextField!
+    
+    @IBOutlet weak var passwordInput: UITextField!
+    
+   override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+                    // Проверяем данные
+                    let checkResult = checkUserData()
+                    
+                    // Если данные не верны, покажем ошибку
+                    if !checkResult {
+                        showLoginError()
+                    }
+                    
+                    // Вернем результат
+                    return checkResult
+                }
+                
+                func checkUserData() -> Bool {
+                    guard let login = loginInput.text,
+                        let password = passwordInput.text else { return false }
+                    
+                    if login == "admin" && password == "123" {
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+                
+
+                func showLoginError() {
+                    // Создаем контроллер
+                    let alter = UIAlertController(title: "Ошибка", message: "Введены не верные данные пользователя.\n\nLogin: admin\nPassword: 123", preferredStyle: .alert)
+                    // Создаем кнопку для UIAlertController
+                    let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    // Добавляем кнопку на UIAlertController
+                    alter.addAction(action)
+                    // Показываем UIAlertController
+                    present(alter, animated: true, completion: nil)
+                }
+    @IBAction func enter(_ sender: Any) {
+    
+        
+    }
+   
+
+
+}
